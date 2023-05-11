940d0a831d3796d6802024b756816350013b94f1
tvcuong212 <133182272+tvcuong212@users.noreply.github.com>
2023-05-11 10:59:56 +0700

    Create 110523_Baitap_03.c

---
+++ 110523_Baitap_03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+main()
+{
+
+    int num1, num2;
+
+    printf("Nhap so num1 = ");
+    scanf("%d", &num1);
+    printf("Nhap so num2 = ");
+    scanf("%d", &num2);
+    
+    printf("Tong = %d\n", num1+num2);
+    printf("Hieu = %d\n", num1-num2);
+    printf("Tich = %d\n", num1*num2);
+    printf("Thuong = %d\n", num1/num2);
+    
+    return 0;
+}
