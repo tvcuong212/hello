bf9b6f96f3bc5f8967539ef910f1f45b43fe0e26
tvcuong212 <133182272+tvcuong212@users.noreply.github.com>
2023-05-11 10:18:07 +0700

    110523_Baitap_01.c

---
+++ 110523_Baitap_01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+  
+    printf("-------------------------------\n");
+    printf("* Class: T1104L               *\n");
+    printf("* RolNo: B0001                *\n");
+    printf("* Full Name: Nguyen Van A     *\n");
+    printf("* Email: anv@fpt.aptech.ac.vn *\n");
+    printf("* Phone: 0912345678           *\n");
+    printf("-------------------------------\n");
+
+}
