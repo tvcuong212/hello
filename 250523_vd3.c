95f56f90e6c09d8b2c81f03d95e6c4891f2392c3
tvcuong212 <133182272+tvcuong212@users.noreply.github.com>
2023-05-25 09:23:57 +0700

    Create 250523_vd3.c

---
+++ 250523_vd3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main() {
+    int ary[5];
+    int i, min, max, tong=0;
+    float trungbinh;
+    for (i = 0; i < 5; i++) {
+        printf("Nhap vao phan tu %d: ", i+1);
+        scanf("%d", &ary[i]);
+    }
+    
+    max = ary [0];
+    min = ary [0];
+    for (i = 1; i < 5; i++){
+        {
+            if (ary[i] > max)
+                max = ary[i];
+            else if (ary[i] < min)
+                min = ary[i];
+        }
+        tong += ary[i];
+        trungbinh = (float)tong/i;
+    }
+    printf("So lon nhat la %d", max);
+    printf("\nSo be nhat la %d\n", min);
+    printf("Tong cac so la %d\n", tong);
+    printf("Trung binh cac so la %3f2\n", trungbinh);
+
+        }
